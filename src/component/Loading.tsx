592cd9f3573dc644ef14ed8c6f5c021ea8529ec0
skorea6 <skorea6@naver.com>
2024-01-16 22:02:27 +0900

    Loading 추가

---
+++ src/component/Loading.tsx
@@ -0,0 +1,9 @@
+import { Background, LoadingText } from "./Styles";
+
+export default function Loading() {
+  return (
+    <Background>
+      <LoadingText>잠시만 기다려 주세요.</LoadingText>
+    </Background>
+  );
+}
