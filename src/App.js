74beca73bf5124b18421963d624c5cf6fd65f70f
skorea6 <skorea6@naver.com>
2024-01-18 13:29:52 +0900

    App.js에 LoadingContext로 감쌈

--- src/App.js
+++ src/App.js
@@ -5,22 +5,35 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EmptyPage from "./component/EmptyPage";
 import CreateWord from "./component/CreateWord";
 import CreateDay from "./component/CreateDay";
+import Loading from "./component/Loading";
+import {
+  LoadingContextProvider,
+  useLoadingContext,
+} from "./component/LoadingContext";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<DayList />} />
-          <Route path="/day/:day" element={<Day />} />
-          <Route path="/create_word" element={<CreateWord />} />
-          <Route path="/create_day" element={<CreateDay />} />
-          <Route path="/*" element={<EmptyPage />} />
-        </Routes>
+        <LoadingContextProvider>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<DayList />} />
+            <Route path="/day/:day" element={<Day />} />
+            <Route path="/create_word" element={<CreateWord />} />
+            <Route path="/create_day" element={<CreateDay />} />
+            <Route path="/*" element={<EmptyPage />} />
+          </Routes>
+          <LoadingComponent />
+        </LoadingContextProvider>
       </div>
     </BrowserRouter>
   );
 }
+
+const LoadingComponent = () => {
+  const { loading } = useLoadingContext();
+  return loading ? <Loading /> : null;
+};
 
 export default App;
