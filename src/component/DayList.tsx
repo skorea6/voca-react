8dd11d45a285be038417f0f45de3e050631f022d
skorea6 <skorea6@naver.com>
2024-01-18 13:30:10 +0900

    Day, DayList에 Loading 로직 추가

--- src/component/DayList.tsx
+++ src/component/DayList.tsx
@@ -9,10 +9,6 @@ export interface IDay {
 export default function DayList() {
   const days: IDay[] = useFetch("http://localhost:3001/days");
 
-  if (days.length === 0) {
-    return <span>Loading...</span>;
-  }
-
   return (
     <ul className="list_day">
       {days.map((day) => (
